5cd692bcaf7db661d15ba1b86417517fe72e7fa3
rayah97 <harutyunyan.raya97@gmail.com>
2023-01-08 19:06:59 +0400

    added run action11

--- variables.tf
+++ variables.tf
@@ -2,7 +2,10 @@ variable "instance_name" {
   type        = string
   description = "The name tag of the ec2 instance"
 }
-
+variable "instance_id" {
+  type        = string
+  description = "The ID of the ec2 instance"
+}
 output "instance_id" {
   value = aws_instance.myec2.id
 }
