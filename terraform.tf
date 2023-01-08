eb354be1bb91bc9b972850d6a4ce3d2ed2de2f61
rayah97 <harutyunyan.raya97@gmail.com>
2023-01-08 14:30:47 +0400

    added run action11

---
+++ terraform.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    region = "us-east-1"
+}
+resource "aws_instance" "myec2"{ 
+
+   ami = "ami-0b5eea76982371e91" 
+   instance_type = "t2.micro"
+}
+resource "aws_instance" "myec2-new"{ 
+
+   ami = "ami-0b5eea76982371e91" 
+   instance_type = "t2.micro"
+}
