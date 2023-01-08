f1cfecbc8f538875eda2382312b850bb8e96d828
rayah97 <harutyunyan.raya97@gmail.com>
2023-01-08 15:24:43 +0400

    added run action11

--- terraform.tf
+++ terraform.tf
@@ -10,6 +10,10 @@ resource "aws_instance" "myec2"{
 
    ami = "ami-0b5eea76982371e91" 
    instance_type = "t2.micro"
-   name = "${var.instance_name}"
+
+   tags = {
+    Name = "${var.instance_name}"
+  }
+  
 }
 
