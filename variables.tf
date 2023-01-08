eb3c9c0f7f7358a3b1ff4d2f9f6a8be8f56771f1
rayah97 <harutyunyan.raya97@gmail.com>
2023-01-08 20:18:03 +0400

    added run action11

--- variables.tf
+++ variables.tf
@@ -2,7 +2,3 @@ variable "instance_name" {
   type        = string
   description = "The name tag of the ec2 instance"
 }
-variable "instance_id" {
-  type        = string
-  description = "The ID of the EC2 instance"
-}
