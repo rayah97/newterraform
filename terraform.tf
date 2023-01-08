73343931cbcb1b2ae115f1b4d1a0258ea8eafa4d
rayah97 <harutyunyan.raya97@gmail.com>
2023-01-08 15:16:34 +0400

    added run action11

--- terraform.tf
+++ terraform.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-1"
 }
 
-variable "input_name" {
+variable "instance_name" {
   type        = string
   description = "The name tag of the ec2 instance"
 }
@@ -11,7 +11,7 @@ resource "aws_instance" "myec2"{
    ami = "ami-0b5eea76982371e91" 
    instance_type = "t2.micro"
    tags {
-      Name = "${input_name}"
+      Name = "${var.instance_name}"
    }
 }
 
