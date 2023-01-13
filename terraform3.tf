81519b1ef10db424447a98d031469e5a9ff6558b
rayah97 <harutyunyan.raya97@gmail.com>
2023-01-13 22:52:58 +0400

    added run action11

--- terraform3.tf
+++ terraform3.tf
@@ -1,27 +1,23 @@
-resource "aws_s3_bucket" "tf_state_bucket" {
-  bucket = "my-tf-state-bucket"
-  acl    = "private"
+ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket_object" "tf_state_file" {
-  bucket = aws_s3_bucket.tf_state_bucket.id
-  key    = "path/to/my/state/file"
+variable "instance_name" {
+  type        = string
+  description = "The name tag of the ec2 instance"
 }
+resource "aws_instance" "myec2" {
+  ami           = "ami-0b5eea76982371e91"
+  instance_type = "t2.micro"
 
-output "tf_state_bucket_id" {
-  value = aws_s3_bucket.tf_state_bucket.id
+  tags = {
+    Name = "${var.instance_name}"
+  }
 }
-
-output "tf_state_file_key" {
-  value = aws_s3_bucket_object.tf_state_file.key
-}
-
-
 terraform {
   backend "s3" {
-    bucket = aws_s3_bucket.tf_state_bucket.id
-    key    = aws_s3_bucket_object.tf_state_file.key
+    bucket = "bucketforterraformangit"
+    key    = "develop/terraform.tfstate"
     region = "us-east-1"
   }
 }
