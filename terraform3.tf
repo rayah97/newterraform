3aab716d175e9608adffbff18ac04c8ad3c1568f
rayah97 <harutyunyan.raya97@gmail.com>
2023-01-13 18:57:04 +0400

    added run action11

--- terraform3.tf
+++ terraform3.tf
@@ -2,6 +2,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "name" {
+  default = "my-ec2-instance"
+}
+
 resource "aws_s3_bucket" "tf_state_bucket" {
   bucket = "my-tf-state-bucket"
   acl    = "private"
@@ -11,6 +15,15 @@ resource "aws_s3_bucket" "tf_state_bucket" {
 resource "aws_s3_bucket_object" "tf_state_file" {
   bucket = aws_s3_bucket.tf_state_bucket.id
   key    = "path/to/my/state/file"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0e73a05f4e04a71f1"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.name
+  }
 }
 
 terraform {
