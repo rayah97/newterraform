f2fb2c2698a75fa1e5eb7f9badcb1e29a204b976
rayah97 <harutyunyan.raya97@gmail.com>
2023-01-14 15:19:11 +0400

    added run action11

--- terraform3.tf
+++ terraform3.tf
@@ -1,23 +1,25 @@
- provider "aws" {
-  region = "us-east-1"
-}
+#  provider "aws" {
+#   region = "us-east-1"
+# }
 
-variable "instance_name" {
-  type        = string
-  description = "The name tag of the ec2 instance"
-}
-resource "aws_instance" "myec2" {
-  ami           = "ami-0b5eea76982371e91"
-  instance_type = "t2.micro"
+# variable "instance_name" {
+#   type        = string
+#   description = "The name tag of the ec2 instance"
+# }
+# resource "aws_instance" "myec2" {
+#   ami           = "ami-0b5eea76982371e91"
+#   instance_type = "t2.micro"
 
-  tags = {
-    Name = "${var.instance_name}"
-  }
-}
-terraform {
-  backend "s3" {
-    bucket = "bucketforterraformangit"
-    key    = "develop/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
+#   tags = {
+#     Name = "${var.instance_name}"
+#   }
+# }
+
+
+# terraform {
+#   backend "s3" {
+#     bucket = "bucketforterraformangit"
+#     key    = "develop/terraform.tfstate"
+#     region = "us-east-1"
+#   }
+# }
---
+++ terraform4.tf
@@ -0,0 +1,42 @@
+ provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "tf_state_bucket" {
+  bucket = "my-tf-state-bucket"
+  acl    = "private"
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket_object" "tf_state_file" {
+  bucket = aws_s3_bucket.tf_state_bucket.id
+  key    = "develop/"
+}
+
+variable "instance_name" {
+  type        = string
+  description = "The name tag of the ec2 instance"
+}
+resource "aws_instance" "myec2" {
+  ami           = "ami-0b5eea76982371e91"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "${var.instance_name}"
+  }
+}
+output "tf_state_bucket_id" {
+  value = aws_s3_bucket.tf_state_bucket.id
+}
+
+output "tf_state_file_key" {
+  value = aws_s3_bucket_object.tf_state_file.key
+}
+
+terraform {
+  backend "s3" {
+    bucket = var.tf_state_bucket_id
+    key    = var.tf_state_file_key
+    region = "us-east-1"
+  }
+}
